0f4c49a5c88dcb9fc202953b0590f8639174f14e
Mehmet Ubeyde Kara <ubeydekara@yahoo.com>
2023-09-19 20:04:35 +0300

    refactor

--- src/component/LoginVector.js
+++
@@ -1,8 +0,0 @@
-import {SvgIcon} from "@mui/material";
-import LoginSvg from '../asset/login.svg';
-
-export default function LoginVector() {
-    return(
-        <SvgIcon component={LoginSvg} inheritViewBox />
-    )
-}
